51776cbd59de50393dd43d38c6591ccb9102bba9
Brian Ries <bkries@gmail.com>
2015-07-02 21:12:36 -0400

    F to C button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+function convertToC() {
+
+	 //on click, get user input
+	var f = $('.userTemp').val();
+	console.log(f);
+
+ //value of userTemp intput is string
+ //need to convert String to Int
+	f = parseInt(f);
+
+  //convert user input (F) to C
+  //C = (f - 32) x (5/9)
+ 	var cTemp = (f - 32) * (5/9);
+
+  //display C result
+  	console.log(cTemp);
+  	$('.result').text(cTemp);
+}
+
+
 $(function() {
   // Handler for .ready() called.
+  // psuedo code 
+
+  //detect click
+  $('.convert').click(convertToC);
+
 });
