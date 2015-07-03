dad08a59a86de4ce79184cbdc7b9b39079752399
Brian Ries <bkries@gmail.com>
2015-07-02 23:51:35 -0400

    Javascript -> jQuery tweaks

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
 function convertToC() {
 
-	 //on click, get user input
+	//on click, get user input
 	var f = $('.userTemp').val();
 	console.log(f);
 
- //value of userTemp intput is string
- //need to convert String to Int
+ 	//value of userTemp intput is string
+ 	//need to convert String to Int
 	f = parseInt(f);
 
-  //convert user input (F) to C
-  //C = (f - 32) x (5/9)
+ 	 //convert user input (F) to C
+ 	 //C = (f - 32) x (5/9)
  	var cTemp = (f - 32) * (5/9);
 
-  //display C result
+  	//display C result
   	console.log(cTemp);
   	$('.result').text(cTemp);
 }
 
+	$(function() {
+  	// Handler for .ready() called.
 
-$(function() {
-  // Handler for .ready() called.
-  // psuedo code 
-
-  //detect click
-  $('.convert').click(convertToC);
+  	$('.convert').click(convertToC);
 
 });
+
+	$(function() {
+  	// Handler for .ready() called.
+
+  	$('.convert').click(convertToC)
+
+});
+
